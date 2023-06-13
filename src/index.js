00cfc5b84e60e1209f283c3aff76e27b1ea42e46
kashif tufail <kashif.tufail17@gmail.com>
2023-06-13 18:59:35 +0500

    add   redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/store";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>
   ,
