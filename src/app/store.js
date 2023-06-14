26415c67cae8b8383ad7650199516dd712f86bcb
kashif tufail <kashif.tufail17@gmail.com>
2023-06-14 12:20:24 +0500

    fetching real time data from api

--- src/app/store.js
+++ src/app/store.js
@@ -6,7 +6,7 @@ import { cryptoApi } from '../services/cryptoApi';
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
-
+    // [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
   },  
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(cryptoApi.middleware),
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,14 +4,18 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
+// import Loader from './Loader';
 
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery(10);
+  const { data, isFetching } = useGetCryptosQuery();
+  // console.log(data)
   const globalStats = data?.data?.stats;
 
-  if (isFetching) return 'Loading.....';
+  if (isFetching) return '<Loader />';
 
   return (
     <>
@@ -24,7 +28,16 @@ const Homepage = () => {
         <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
         <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
-      
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3}><Link to="/news">Show more</Link></Title>
+      </div>
+      <News simplified />
     </>
   );
 };
