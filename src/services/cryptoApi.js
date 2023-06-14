1baa7dd4d48f9600bc4aa92e2a4e482e12da8133
kashif tufail <kashif.tufail17@gmail.com>
2023-06-14 12:50:18 +0500

    add limit count in query

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -18,7 +18,7 @@ export const cryptoApi = createApi({
   endpoints: (builder) => ({
     getCryptos: builder.query({
       
-      query: () => createRequest('/coins')
+      query: (count) => createRequest(`/coins?limit=${count}`)
     }),
   }),
 });
